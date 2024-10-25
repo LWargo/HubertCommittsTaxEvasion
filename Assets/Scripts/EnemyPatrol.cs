aca6c2fcbf2ab2faa5d8be28e9a13990134d1bab
Preston Harris <pharris@trinity.edu>
2024-10-25 12:12:57 -0500

    making casey's enemy stuff work with maze stuff

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -17,14 +17,18 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if at patrol point, cycle to next patrol point
         if(transform.position == patrolPoints[targetPoint].position) {
             IncreaseTargetInt();
         }
+        // move from current position toward next patrol point every update
         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
     }
 
     void IncreaseTargetInt() {
         targetPoint++;
+        transform.Rotate(0, 0, -90);
+        // if the enemy has reached all patrol points, loop through all patrol points again
         if(targetPoint >= patrolPoints.Length) {
             targetPoint = 0;
         }
