700e4bb9970bd278669e0d1c0e18fd7938d3484f
Lwargomake <lwargo@trinity.edu>
2024-10-27 21:49:02 -0500

    ice cream count + enemy collision restart

--- Assets/Scripts/HubertBehavior.cs
+++ Assets/Scripts/HubertBehavior.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using TMPro;
 
 // chatgpt influenced
 public class HubertBehavior : MonoBehaviour
@@ -8,17 +11,27 @@ public class HubertBehavior : MonoBehaviour
     public float moveSpeed = 5f;  // Adjust this for your desired speed
     private Rigidbody2D rb;
     private Vector2 movement;
+    public EnemyPatrol enemyPatrol;
+    public Vector3 startpos;
+    public TMP_Text icc_txt;
+    int icc = 0;
+    public GameObject iceCream;
 
     void Start()
     {
+      //  icc_txt = GetComponent<TMP_Text>();
         rb = GetComponent<Rigidbody2D>();
+        startpos = this.transform.position;
+        icc_txt.SetText( "Ice Cream Count: " + icc);
     }
+    
 
     void Update()
     {
         // Get input from the player
         movement.x = Input.GetAxisRaw("Horizontal"); // Left and Right keys (or A/D keys)
         movement.y = Input.GetAxisRaw("Vertical");   // Up and Down keys (or W/S keys)
+        
     }
 
     void FixedUpdate()
@@ -32,6 +45,15 @@ public class HubertBehavior : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Powerup")) {
             Destroy(other.gameObject);
+        }
+        if(other.gameObject.CompareTag("Enemy")){
+            this.transform.position = startpos; //move player back to start
+            icc = 0;
+            icc_txt.text="Ice Cream Count:" + icc;
+        }
+        if(other.gameObject.CompareTag("Powerup")){
+            icc++;
+            icc_txt.text = "Ice Cream Count: " + icc;
         }
     }
 }
